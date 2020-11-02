9c6911e917b979d3a7bf6b7cc30e7a24487de316
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-02 13:00:24 +0530

    added csv

---
+++ src/main/java/com/aiocdawacs/bulkorderhousekeeping/BulkOrderHousekeepingApplication.java
@@ -0,0 +1,13 @@
+package com.aiocdawacs.bulkorderhousekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkOrderHousekeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkOrderHousekeepingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiocdawacs/bulkorderhousekeeping/BulkOrderHousekeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiocdawacs.bulkorderhousekeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkOrderHousekeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
